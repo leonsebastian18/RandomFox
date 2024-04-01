b8def50043195a10946e760e66f3c5c85fbf9fae
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-04-01 14:20:31 -0500

    se añadio el botón para aleatorizar las imagenes, se añadio un generador de id para cada imagen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import type { NextPage } from "next";
 import Head from 'next/head';
 import { RandomFox } from "./components/RandomFox"; 
@@ -7,15 +7,28 @@ import { RandomFox } from "./components/RandomFox";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 
+//generate simple unique id
+const generateId = () => Math.random().toString(36).substr(2,9);
+
+type ImageItem = {id: string, url: string}
+
 const Home: NextPage = () => {
 
  
-  const [images, setImages] = useState<Array<string>>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-  ]);
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox : MouseEventHandler<HTMLButtonElement> = (event) => {
+    
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    };
+
+    setImages([
+      ...images,
+      newImageItem
+    ])
+  }
 
   return (
     <div>
@@ -26,9 +39,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        {images.map((image, index) => (
-          <div className="p-4" key={index}>
-            <RandomFox image={image} />
+        <h1 className="text-3xl font-bold underline"> Hello Platzi!</h1>
+        <button onClick={addNewFox}> Add new fox</button>
+        {images.map(({id, url}) => (
+          <div className="p-4" key={id}>
+            <RandomFox image={url} />
           </div>
         ))}
       </main>
