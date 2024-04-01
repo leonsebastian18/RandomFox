e875c7bbe98b2ff99067ed906d6578b560eef08c
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-04-01 15:03:17 -0500

    se añadió lazy loading usando useEffect y useState

--- app/components/RandomFox.tsx
+++ app/components/RandomFox.tsx
@@ -1,6 +1,38 @@
+import { useRef, useEffect, useState } from "react";
+
 type Props = { image: string};
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
+
+    const node = useRef<HTMLImageElement>(null);
+    const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+    // nuevo observador
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                //onIntersection-->console.log
+                if (entry.isIntersecting) {
+                    setSrc(image)
+                }
+                
+            });
+        });
+           
     
-    return <img width={320} height="auto" src={image} className="rounded" />
+    
+        //observe node
+        if (node.current) {
+            observer.observe(node.current);
+        }
+    
+        // desconectar
+        return () => {
+            observer.disconnect()
+        }
+    }, [image]);
+    
+    return <img ref={node} width={320} height="auto" src={src} className="rounded bg-gray-300" />
 }
